38f74ddda1bee4ae20e63a5f6eb4f5d5179ecd8c
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2020-12-23 02:35:47 +0600

    Added LOG-IN Sign-UP

---
+++ UserHandler/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
+
+# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        firstName = request.POST.get("first_name")
+        lastName = request.POST.get("last_name")
+        userName = request.POST.get("username")
+        email = request.POST.get("email")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+        if password1 == password2:
+            password = password2
+        else:
+            return HttpResponse("<h1>403 not Allowed</h1>")
+        user = User.objects.create_user(userName, email, password)
+        user.last_name = lastName
+        user.first_name = firstName
+        user.save()
+    return render(request, "front/sign-up/index.html")
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            return redirect("/")
+        else:
+            return HttpResponse("<h1>User not authenticated</h1>")
+    return render(request, 'front/log-in/index.html')
